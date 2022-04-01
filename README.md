deb4ad94284f6715ecba7d1f5fef24d26d8115be
the-runtime <87934058+the-runtime@users.noreply.github.com>
2022-04-01 02:21:31 -0400

    Add files via upload
    hello

---
+++ README.md
@@ -0,0 +1,19 @@
+# github_downloader
+This python program will let you download specific folders from any  github repository.
+
+
+Required python libraries  :
+     
+     
+    pip install selectorlib requests 
+     
+     
+ 
+ ## usage:
+ 
+     python github_downloader <url of github folder> <path where folder needs to be downloaded>
+
+This project is still in development so as of now
+ it cannot download whole repo or just a  single file(only a folder i.e. all internal folders and it's all files)
+ It will give errors when a non github link is provided.
+ 
---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+import github_downloader
+from routerequest import req
+from fastapi.responses import StreamingResponse
+from starlette.background import BackgroundTasks
+import os
+import shutil
+
+def remove_file(dir_name) -> None:
+    os.remove(dir_name + ".zip")
+    shutil.rmtree(dir_name)
+
+app = FastAPI()
+
+@app.post("/url")
+def  hello(Req:req, background_tasks: BackgroundTasks):
+    url = Req.url
+    dir_name = github_downloader.run(url)
+
+    def iterfile():  #
+
+        with open(dir_name+".zip", mode="rb") as file_like:  #
+
+            yield from file_like  #
+
+
+
+    resp = StreamingResponse(iterfile(), media_type="application/x-zip-compressed", headers={
+        'Content-Disposition': f'attachment;filename={dir_name}'
+    })
+
+    background_tasks.add_task(remove_file, dir_name)
+    return resp
