6a75d4b77240073ecc2cb211380929471579f43e
the-runtime <87934058+the-runtime@users.noreply.github.com>
2022-06-05 01:36:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+import github_downloader
+#from routerequest import req
+from fastapi.responses import StreamingResponse
+from starlette.background import BackgroundTasks
+import os
+import shutil
+
+def remove_file(dir_name) -> None:
+    os.remove(dir_name + ".zip")
+    shutil.rmtree(dir_name)
+
+app = FastAPI()
+
+@app.get("/check")
+def check():
+    return {"hello","bye"}
+
+@app.get("/url/{url1}")
+def  hello(url1: str, background_tasks: BackgroundTasks):
+    #url1 = Req.url
+    url = url1
+    dir_name = github_downloader.run(url)
+
+    def iterfile():  #
+
+        with open(dir_name+".zip", mode="rb") as file_like:  #
+
+            yield from file_like  #
+
+
+
+    resp = StreamingResponse(iterfile(), media_type="application/x-zip-compressed", headers={
+        'Content-Disposition': f'attachment;filename={dir_name}'
+    })
+
+    background_tasks.add_task(remove_file, dir_name)
+    return resp
