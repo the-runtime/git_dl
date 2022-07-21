3674e813498687e31f47d848741e385950177556
the-runtime <theruntime7@gmail.com>
2022-07-22 01:01:15 +0530

    chage

--- github_downloader.py
+++ github_downloader.py
@@ -1,49 +1,47 @@
 import shutil
 from selectorlib import Extractor
-import requests 
+import requests
 import os
 import io
+
 
 def per20(st):
     t = st.split(" ")
     r_st = ""
     for r in t:
-        r_st = r_st +"/"+ r
+        r_st = r_st + "/" + r
     print(r_st)
 
     return r_st
 
 
 def fit(t):
-  i=0
-  while i<len(t):
-     if ( (t[i]["link"].find("/tree/")+1) and (t[i]["name"]!=None) ) :
-        t[i]['type']='folder'
-     elif (t[i]["link"].find("/blob/")+1):
-        r1=t[i]["link"].find("/blob/")
-        s=t[i]["link"][0:r1]+t[i]["link"][r1+5:]
-        t[i]["link"]=s
-        t[i]['type']='file'
-     else :
-        t[i]['type']='move_up'
-     i=i+1
- 
- 
-def rel(t,master):
-  for r in t:
-    if r['type']=="folder":
-        data=scrape("https://github.com"+r["link"])
-        t=data["branches"]
-        fit(t)
-        rel(t,master)
-        master.append(t)
- 
+    i = 0
+    while i < len(t):
+        if (t[i]["link"].find("/tree/") + 1) and (t[i]["name"] is not None):
+            t[i]['type'] = 'folder'
+        elif t[i]["link"].find("/blob/") + 1:
+            r1 = t[i]["link"].find("/blob/")
+            s = t[i]["link"][0:r1] + t[i]["link"][r1 + 5:]
+            t[i]["link"] = s
+            t[i]['type'] = 'file'
+        else:
+            t[i]['type'] = 'move_up'
+        i = i + 1
 
 
+def rel(t, master):
+    for r in t:
+        if r['type'] == "folder":
+            data = scrape("https://github.com" + r["link"])
+            t = data["branches"]
+            fit(t)
+            rel(t, master)
+            master.append(t)
 
 
-def scrape(url): 
-    e = Extractor.from_yaml_file('selector.yml')   
+def scrape(url):
+    e = Extractor.from_yaml_file('selector.yml')
     headers = {
         'authority': 'www.github.com/',
         'pragma': 'no-cache',
@@ -58,78 +56,69 @@ def scrape(url):
         'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     }
     # Download the page using requests
-    print("Reading page: %s"%url)
+    print("Reading page: %s" % url)
     r = requests.get(url, headers=headers)
     # Simple check to check if page was blocked (Usually 503)
     if r.status_code > 500:
         if "To discuss automated access to flipkart data please contact" in r.text:
-            print("Page %s was blocked by flipkart. Please try using better proxies\n"%url)
+            print("Page %s was blocked by flipkart. Please try using better proxies\n" % url)
         else:
-            print("Page %s must have been blocked by flipkart as the status code was %d"%(url,r.status_code))
+            print("Page %s must have been blocked by flipkart as the status code was %d" % (url, r.status_code))
         return None
     # Pass the HTML of the page and create 
     return e.extract(r.text)
 
 
-
 def run(url):
-       
+    def diz(u):
+        nex = u.find(urt) + len(urt)
+        return u[nex:]
 
-        def diz(u):
-         nex=u.find(urt)+len(urt)
-         return(u[nex:])
-    
-        
-        #url = per20(per_url)
-        print("hellonknjfb")
-        #url = "https:/"+url
-        urt=url
-        while True :
-            ur=urt.find("/")+1
-            if ur:
-                    urt=urt[ur:]
-            else :
-                break
-                
-        print(urt)
+    # url = per20(per_url)
+    print("hellonknjfb")
+    # url = "https:/"+url
+    urt = url
+    while True:
+        ur = urt.find("/") + 1
+        if ur:
+            urt = urt[ur:]
+        else:
+            break
 
+    print(urt)
 
-        data=scrape(url)
-        t=data["branches"]
-        fit(t)
+    data = scrape(url)
+    t = data["branches"]
+    fit(t)
 
-        master=[]
-        master.append(t)
-        rel(t,master)
+    master = [t]
+    rel(t, master)
 
-        
+    # try:
+    #   os.mkdir(path)
+    # except:
+    #   print("hello control passed from except")
+    # os.mkdir(os.path.join(path,urt))
+    # urt1=path+"/"+urt
+    urt1 = urt
+    dir_name = urt
+    os.mkdir(dir_name)
+    for r in master:
+        for r1 in r:
+            if r1["type"] == "folder":
+                os.mkdir(urt1 + diz(r1["link"]))
 
-        #try:
-         #   os.mkdir(path)
-        #except:
-         #   print("hello control passed from except")
-        #os.mkdir(os.path.join(path,urt))
-        #urt1=path+"/"+urt
-        urt1 =  urt
-        dir_name = urt
-        os.mkdir(dir_name)
-        for r in master:
-            for r1 in r:
-                if r1["type"]=="folder":
-                    os.mkdir(urt1+diz(r1["link"]))
-                    
-                
-        for r in master:
-            for r1 in r:
-                if r1["type"]=="file":
-                    try:
-                        print("downloading file: %s"%r1["name"])
-                        myfile=requests.get("https://raw.githubusercontent.com"+r1["link"])  
-                        open( urt1+diz(r1["link"]), 'wb').write(myfile.content)
-                    except:
-                        print("there is some problem with that file")
-                        continue
-                        
-        shutil.make_archive(dir_name, 'zip', dir_name)
+    for r in master:
+        for r1 in r:
+            if r1["type"] == "file":
+                try:
+                    print("downloading file: %s" % r1["name"])
+                    myfile = requests.get("https://raw.githubusercontent.com" + r1["link"])
+                    open(urt1 + diz(r1["link"]), 'wb').write(myfile.content)
+                except:
+                    print("there is some problem with that file")
+                    continue
 
-        return dir_name
+    shutil.make_archive(dir_name, 'zip', dir_name)
+
+    return dir_name
--- main.py
+++ main.py
@@ -9,69 +9,60 @@ import shutil
 
 app = FastAPI()
 
+
 def remove_file(dir_name) -> None:
     os.remove(dir_name + ".zip")
     shutil.rmtree(dir_name)
 
 
-
-
-@app.get("/",response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 def start_page():
-    fi_html = open("README.html","r")
+    fi_html = open("README.html", "r")
     data = fi_html.read()
     fi_html.close()
-    return  data
-
+    return data
 
 
 @app.get("/check")
 def check():
-    return {"hello","bye"}
+    return {"hello", "bye"}
 
 
 @app.post("/get_add")
 def get_add(REQ: req):
     url = REQ.url
     dir_name = github_downloader.run(url)
-    
 
     return dir_name
 
 
-
 @app.get("/download/{name}")
-def download(name:str, background_tasks: BackgroundTasks):
+def download(name: str, background_tasks: BackgroundTasks):
     dir_name = name
-    def iterfile():
-        
-        with open(dir_name+".zip",mode='rb') as file_like:
 
+    def iterfile():
+        with open(dir_name + ".zip", mode='rb') as file_like:
             yield from file_like
 
-    resp = StreamingResponse(iterfile(), media_type="application/x-zip-compressed", headers={                                   
-           'Content-Disposition': f'attachment;filename={dir_name}'                                                                
-       })
-    
+    resp = StreamingResponse(iterfile(), media_type="application/x-zip-compressed", headers={
+        'Content-Disposition': f'attachment;filename={dir_name}'
+    })
+
     background_tasks.add_task(remove_file, dir_name)
     return resp
 
 
-
-
 @app.get("/url/{url1}")
-def  hello(url1: str, background_tasks: BackgroundTasks):
-    #url1 = Req.url
+def hello(url1: str, background_tasks: BackgroundTasks):
+    # url1 = Req.url
     url = github_downloader.per20(url1)
     dir_name = github_downloader.run(url)
 
     def iterfile():  #
 
-        with open(dir_name+".zip", mode="rb") as file_like:  #
+        with open(dir_name + ".zip", mode="rb") as file_like:  #
 
             yield from file_like  #
-
-
 
     resp = StreamingResponse(iterfile(), media_type="application/x-zip-compressed", headers={
         'Content-Disposition': f'attachment;filename={dir_name}'
