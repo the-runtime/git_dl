b9bbe576f94b93efd11e2f62c844f8757b6dad37
the runtime <theruntime7@gmail.com>
2023-06-24 12:05:10 +0530

    cli prototype is working

---
+++ cli_main.py
@@ -0,0 +1,8 @@
+from github_downloader import run
+import sys
+
+if len(sys.argv) > 1:
+    url = sys.argv[1]
+    run(url)
+else:
+    print("Enter a gihub repo")
--- website/assets/js/control.js
+++
@@ -1,7 +0,0 @@
-button = document.getElementById('button');
-button.addEventListener("click", clicked);
-function clicked(){
-    link = document.getElementById('link').value;
-    console.log(link);
-    window.location.replace("url/?link="+link);
-};
