583eae6033a61b25203b67c5b1741f782fc8f56d
Restyled.io <commits@restyled.io>
2023-07-13 07:14:25 +0000

    Restyled by black

--- git_dl/github_downloader.py
+++ git_dl/github_downloader.py
@@ -19,20 +19,20 @@ def fit(t):
     i = 0
     while i < len(t):
         if (t[i]["link"].find("/tree/") + 1) and (t[i]["name"] is not None):
-            t[i]['type'] = 'folder'
+            t[i]["type"] = "folder"
         elif t[i]["link"].find("/blob/") + 1:
             r1 = t[i]["link"].find("/blob/")
-            s = t[i]["link"][0:r1] + t[i]["link"][r1 + 5:]
+            s = t[i]["link"][0:r1] + t[i]["link"][r1 + 5 :]
             t[i]["link"] = s
-            t[i]['type'] = 'file'
+            t[i]["type"] = "file"
         else:
-            t[i]['type'] = 'move_up'
+            t[i]["type"] = "move_up"
         i = i + 1
 
 
 def rel(t, master):
     for r in t:
-        if r['type'] == "folder":
+        if r["type"] == "folder":
             data = scrape("https://github.com" + r["link"])
             t = data["branches"]
             fit(t)
@@ -41,19 +41,19 @@ def rel(t, master):
 
 
 def scrape(url):
-    e = Extractor.from_yaml_file('data/selector.yml')
+    e = Extractor.from_yaml_file("data/selector.yml")
     headers = {
-        'authority': 'www.github.com/',
-        'pragma': 'no-cache',
-        'cache-control': 'no-cache',
-        'dnt': '1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'none',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-dest': 'document',
-        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        "authority": "www.github.com/",
+        "pragma": "no-cache",
+        "cache-control": "no-cache",
+        "dnt": "1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "sec-fetch-site": "none",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-dest": "document",
+        "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
     }
     # Download the page using requests
     print("Reading page: %s" % url)
@@ -62,10 +62,14 @@ def scrape(url):
     if r.status_code > 500:
         if "To discuss automated access to flipkart data please contact" in r.text:
             print(
-                "Page %s was blocked by flipkart. Please try using better proxies\n" % url)
+                "Page %s was blocked by flipkart. Please try using better proxies\n"
+                % url
+            )
         else:
-            print("Page %s must have been blocked by flipkart as the status code was %d" % (
-                url, r.status_code))
+            print(
+                "Page %s must have been blocked by flipkart as the status code was %d"
+                % (url, r.status_code)
+            )
         return None
     # Pass the HTML of the page and create
     return e.extract(r.text)
@@ -116,12 +120,13 @@ def run(url):
                 try:
                     print("downloading file: %s" % r1["name"])
                     myfile = requests.get(
-                        "https://raw.githubusercontent.com" + r1["link"])
-                    open(urt1 + diz(r1["link"]), 'wb').write(myfile.content)
+                        "https://raw.githubusercontent.com" + r1["link"]
+                    )
+                    open(urt1 + diz(r1["link"]), "wb").write(myfile.content)
                 except:
                     print("there is some problem with that file")
                     continue
 
-    shutil.make_archive(dir_name, 'zip', dir_name)
+    shutil.make_archive(dir_name, "zip", dir_name)
 
     return dir_name
--- setup.py
+++ setup.py
@@ -1,34 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='git_dl',
-    version='0.0.1',
-    author='the runtime',
-    author_email='theruntime7@gmail.com',
-    description='Cli app to download github repo folders',
+    name="git_dl",
+    version="0.0.1",
+    author="the runtime",
+    author_email="theruntime7@gmail.com",
+    description="Cli app to download github repo folders",
     # long_description='A longer description of your app',
-    url='https://github.com/the-runtime/git_dl',
+    url="https://github.com/the-runtime/git_dl",
     packages=find_packages(),
-    package_data={
-        'git_dl': ['data/*.yml']
-    },
-    entry_points={
-        'console_scripts': [
-            'git_dl = git_dl.cli_main:start'
-        ]
-    },
-    install_requires=[
-        'requests>=2.25.1',
-        'selectorlib'
-    ],
+    package_data={"git_dl": ["data/*.yml"]},
+    entry_points={"console_scripts": ["git_dl = git_dl.cli_main:start"]},
+    install_requires=["requests>=2.25.1", "selectorlib"],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
